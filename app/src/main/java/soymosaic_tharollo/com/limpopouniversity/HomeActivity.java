1d07a7458adc51c9949798ecca45694fd06a7d91
sontaga <sonti96@live.co.uk>
2018-09-11 21:36:12 +0200

    Initial Commit

---
+++ app/src/main/java/soymosaic_tharollo/com/limpopouniversity/HomeActivity.java
@@ -0,0 +1,13 @@
+package soymosaic_tharollo.com.limpopouniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
